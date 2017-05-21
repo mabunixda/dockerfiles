168193f98a629bbb43bbec8507ba540a20af9bc0
Martin Buchleitner <mbuchleitner@mgit.at>
2017-05-21 20:45:38 +0200

    updating loxone-sonos config
    Former-commit-id: 50ff0c2b5ba24999384b8fa856372c6a473dbda2

--- loxone-sonos/run.sh
+++ loxone-sonos/run.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
+if [ -f /data/sonos_nolb.cfg ]; then
+    echo "using custom sonos config"
+    rm -f /loxone-sonos/webfrontend/html/system/sonos_nolb.cfg
+    ln -sf /data/sonos_nolb.cfg /loxone-sonos/webfrontend/html/system/sonos_nolb.cfg
+fi
+if [ -f /data/player_nolb.cfg ]; then
+    echo "using custom player config"
+    rm -f /loxone-sonos/webfrontend/html/system/player_nolb.cfg
+    ln -sf /data/player_nolb.cfg /loxone-sonos/webfrontend/html/system/player_nolb.cfg
+fi
+
 chown -R $UID:$GID /loxone-sonos /etc/nginx /etc/php7 /var/log /var/lib/nginx /tmp /etc/s6.d
 exec su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
