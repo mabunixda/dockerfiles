30cb0bc44a78f24cb07ddf1eea85d70266b37930
Martin Buchleitner <mbuchleitner@mgit.at>
2017-07-12 08:51:06 +0200

    adding ssl nginx for tests
    Former-commit-id: 2a2fc8a413c7b5cb1b33259ef8ad511a7ac9c227

--- atlassian-sdk/ssl.sh
+++ atlassian-sdk/ssl.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 mkdir /etc/nginx/ssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
+keytool -import -trustcacerts -alias nginx -file /etc/nginx/ssl/nginx.crt -keystore /usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/lib/security/cacerts
+
 nginx
