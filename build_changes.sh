e626917a3fdb5bc608237fcbbeee95383a5b674f
Martin Buchleitner <mbuchleitner@infralovers.com>
2019-09-19 23:40:20 +0200

    update pipelines
    Signed-off-by: Martin Buchleitner <mbuchleitner@infralovers.com>

--- build_changes.sh
+++ build_changes.sh
@@ -3,8 +3,7 @@
 if [ -z "$CI" ]; then
   exit 0
 fi
-CHANGES=$(git diff-tree --no-commit-id --name-only -r $BITBUCKET_COMMIT | xargs dirname | sort | uniq)
-for d in $( $CHANGES  ); do
+for d in $(git diff-tree --no-commit-id --name-only -r $BITBUCKET_COMMIT | xargs dirname | sort | uniq); do
   if [ -f "${d}/Dockerfile" ]; then
     ./build_all.sh dofile "${d}/Dockerfile"
   fi
