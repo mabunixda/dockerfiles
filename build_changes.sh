0d48dd6e1b44499b87835cfe3af5f0dd7e12e665
Martin Buchleitner <mbuchleitner@infralovers.com>
2019-09-25 08:24:45 +0200

    fix dockerfiles and build bucket
    Signed-off-by: Martin Buchleitner <mbuchleitner@infralovers.com>

--- build_changes.sh
+++ build_changes.sh
@@ -3,7 +3,8 @@
 if [ -z "$CI" ]; then
   exit 0
 fi
-for d in $(git diff-tree --no-commit-id --name-only -r $BITBUCKET_COMMIT | xargs dirname | sort | uniq); do
+
+for d in $(for f in $(git diff-tree --no-commit-id --name-only -r $BITBUCKET_COMMIT); do echo $(dirname $f); done | sort | uniq ); do
   if [ -f "${d}/Dockerfile" ]; then
     ./build_all.sh dofile "${d}/Dockerfile"
   fi
