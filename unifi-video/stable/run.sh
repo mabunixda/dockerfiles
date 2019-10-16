7cf2e3a39c9053aa02654099c62fe12544b32307
Martin Buchleitner <mbuchleitner@infralovers.com>
2019-10-16 20:04:33 +0200

    update images overall foo
    Signed-off-by: Martin Buchleitner <mbuchleitner@infralovers.com>

--- unifi-video/stable/run.sh
+++ unifi-video/stable/run.sh
@@ -2,8 +2,18 @@
 
 DATADIR=${DATADIR:-/var/lib/unifi-video}
 BASEDIR="/usr/lib/unifi-video"
+JVM_MAX_MEM=${JVM_MAX_MEM:-512m}
+MAINCLASS="com.ubnt.airvision.Main"
+JVM_JAR="/usr/lib/unifi-video/lib/airvision.jar"
+MONGOPORT=27117
+MONGOLOCK="${DATADIR}/db/mongod.lock"
 
-if [ ! -f "${DATADIR}/system.properties" ]; then
+JVM_OPTS="-Djava.security.egd=file:/dev/urandom -Xmx${JVM_MAX_MEM} -XX:+HeapDumpOnOutOfMemoryError -XX:+UseG1GC "
+JVM_OPTS="${JVM_OPTS} -XX:+UseStringDeduplication -Djava.library.path=${BASEDIR}/lib -Djava.awt.headless=true "
+JVM_OPTS="${JVM_OPTS} -Djavax.net.ssl.trustStore=${DATADIR}/ufv-truststore -Dfile.encoding=UTF-8 "
+confFile="${DATADIR}/system.properties"
+
+if [ ! -f "$confFile" ]; then
 	cp ${BASEDIR}/etc/system.properties ${DATADIR}
 	cp ${BASEDIR}/etc/ufv-truststore ${DATADIR}
 fi
@@ -12,17 +22,53 @@ if [ ! -d "${DATADIR}/videos" ]; then
 	mkdir -p "${DATADIR}/logs"
 fi
 
+
+confSet () {
+  file=$1
+  key=$2
+  value=$3
+
+  if grep -q "^${key} *=" "$file"; then
+    ekey=$(echo "$key" | sed -e 's/[]\/$*.^|[]/\\&/g')
+    evalue=$(echo "$value" | sed -e 's/[\/&]/\\&/g')
+    sed -i "s/^\(${ekey}\s*=\s*\).*$/\1${evalue}/" "$file"
+  else
+    echo "${key}=${value}" >> "$file"
+  fi
+}
+declare -A settings
+if ! [[ -z "$DB_URI" || -z "$STATDB_URI" || -z "$DB_NAME" ]]; then
+  settings["db.mongo.local"]="false"
+  settings["db.mongo.uri"]="$DB_URI"
+  settings["statdb.mongo.uri"]="$STATDB_URI"
+  settings["unifi.db.name"]="$DB_NAME"
+fi
+
+for key in "${!settings[@]}"; do
+  confSet "$confFile" "$key" "${settings[$key]}"
+done
+
+
+exit_handler() {
+    log "Exit signal received, shutting down"
+    $JAVA -jar ${JVM_JAR} stop
+    for i in `seq 1 10` ; do
+        [ -z "$(pgrep -f ${JVM_JAR})" ] && break
+        # graceful shutdown
+        [ $i -gt 1 ] && [ -d ${BASEDIR}/run ] && touch ${BASEDIR}/run/server.stop || true
+        # savage shutdown
+        [ $i -gt 7 ] && pkill -f ${JVM_JAR} || true
+        sleep 1
+    done
+    # shutdown mongod
+    if [ -f ${MONGOLOCK} ]; then
+        mongo localhost:${MONGOPORT} --eval "db.getSiblingDB('admin').shutdownServer()" >/dev/null 2>&1
+    fi
+    exit ${?};
+}
+
+trap 'kill ${!}; exit_handler' SIGHUP SIGINT SIGQUIT SIGTERM
+
 echo "Starting using ${DATADIR}..."
 
-exec java  -cp /usr/lib/unifi-video/lib/airvision.jar  \
-	-Djava.security.egd=file:/dev/urandom \
-	-Xmx1024M \
-	-XX:+HeapDumpOnOutOfMemoryError \
-	-XX:+UseG1GC \
-	-XX:+UseStringDeduplication \
-	-Djava.library.path=${BASEDIR}/lib \
-	-Djava.awt.headless=true \
-	-Djavax.net.ssl.trustStore=${DATADIR}/ufv-truststore \
-	-Dfile.encoding=UTF-8 \
-	com.ubnt.airvision.Main \
-	start
+exec java  -cp ${JVM_JAR} ${JVM_OPTS} ${MAINCLASS} $@
--- unifi/stable/run.sh
+++ unifi/stable/run.sh
@@ -1,15 +1,72 @@
 #!/usr/bin/env bash
 
 JAVA="/usr/bin/java"
-JVM_JAR="/usr/lib/unifi/lib/ace.jar"
-JVM_MAX_MEM=${JVM_MAX_MEM:-512m}
-JVM_DEFAULT_OPTS="-XX:NewRatio=50 -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:+HeapDumpOnOutOfMemoryError -XX:-DisableExplicitGC -XX:+AggressiveOpts -Xnoclassgc -XX:+UseNUMA -XX:+UseFastAccessorMethods -XX:ReservedCodeCacheSize=48m -XX:+UseStringCache -XX:+UseStringDeduplication -XX:MaxGCPauseMillis=400 -XX:GCPauseIntervalMillis=8000"
+BASEDIR="/usr/lib/unifi"
+DATADIR="${BASEDIR}/data"
+LOGSDIR="${DATADIR}/logs"
+RUNDIR="${DATADIR}/run"
 
-JVM_EXTRA_OPTS=${JVM_EXTRA_OPTS}
+MONGOPORT=27117
+MONGOLOCK="${DATADIR}/db/mongod.lock"
+JVM_JAR="${BASEDIR}/lib/ace.jar"
+JVM_MAX_MEM=${JVM_MAX_MEM:-1024m}
+JVM_OPTS="-XX:NewRatio=50 -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:+HeapDumpOnOutOfMemoryError -XX:-DisableExplicitGC -XX:+AggressiveOpts -Xnoclassgc -XX:+UseNUMA "
+JVM_OPTS="${JVM_OPTS} -XX:+UseFastAccessorMethods -XX:+UseStringDeduplication -XX:MaxGCPauseMillis=400 -XX:GCPauseIntervalMillis=8000"
+JVM_OPTS="${JVM_OPTS} -Dunifi.datadir=${DATADIR} -Dunifi.logdir=${LOGSDIR} -Dunifi.rundir=${RUNDIR}"
 
-JVM_OPTS="$JVM_DEFAULT_OPTS"
 if [ ! -z "$JVM_EXTRA_OPTS}" ]; then
   JVM_OPTS="$JVM_OPTS $JVM_EXTRA_OPTS"
 fi
 
-$JAVA -d64 -server -Xmx${JVM_MAX_MEM} ${JVM_OPTS} -jar ${JVM_JAR} $@
+confFile="${DATADIR}/system.properties"
+
+confSet () {
+  file=$1
+  key=$2
+  value=$3
+
+  if grep -q "^${key} *=" "$file"; then
+    ekey=$(echo "$key" | sed -e 's/[]\/$*.^|[]/\\&/g')
+    evalue=$(echo "$value" | sed -e 's/[\/&]/\\&/g')
+    sed -i "s/^\(${ekey}\s*=\s*\).*$/\1${evalue}/" "$file"
+  else
+    echo "${key}=${value}" >> "$file"
+  fi
+}
+declare -A settings
+if ! [[ -z "$DB_URI" || -z "$STATDB_URI" || -z "$DB_NAME" ]]; then
+  settings["db.mongo.local"]="false"
+  settings["db.mongo.uri"]="$DB_URI"
+  settings["statdb.mongo.uri"]="$STATDB_URI"
+  settings["unifi.db.name"]="$DB_NAME"
+fi
+
+for key in "${!settings[@]}"; do
+  confSet "$confFile" "$key" "${settings[$key]}"
+done
+
+exit_handler() {
+    log "Exit signal received, shutting down"
+    $JAVA -jar ${JVM_JAR} stop
+    for i in `seq 1 10` ; do
+        [ -z "$(pgrep -f ${JVM_JAR})" ] && break
+        # graceful shutdown
+        [ $i -gt 1 ] && [ -d ${BASEDIR}/run ] && touch ${BASEDIR}/run/server.stop || true
+        # savage shutdown
+        [ $i -gt 7 ] && pkill -f ${JVM_JAR} || true
+        sleep 1
+    done
+    # shutdown mongod
+    if [ -f ${MONGOLOCK} ]; then
+        mongo localhost:${MONGOPORT} --eval "db.getSiblingDB('admin').shutdownServer()" >/dev/null 2>&1
+    fi
+    exit ${?};
+}
+
+trap 'kill ${!}; exit_handler' SIGHUP SIGINT SIGQUIT SIGTERM
+
+mkdir -p "${LOGSDIR}"
+rm -rf "${BASEDIR}/logs"
+ln -sf "${LOGSDIR}" "${BASEDIR}/logs"
+
+$JAVA -Xmx${JVM_MAX_MEM} ${JVM_OPTS} -jar ${JVM_JAR} $@
